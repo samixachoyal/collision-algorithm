65eb8e5b4658b4dc97a98b781aa71e7da90f5a33
samixachoyal <73836022+samixachoyal@users.noreply.github.com>
2020-11-11 16:45:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedRect,movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "cyan";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "cyan";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0);
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2&&
+    fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2&&
+    movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2&&
+    fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor = "pink";
+    movingRect.shapeColor = "pink";
+
+  }
+  else{
+    movingRect.shapeColor = "cyan";
+    fixedRect.shapeColor = "cyan";
+  }
+  drawSprites();
+}
